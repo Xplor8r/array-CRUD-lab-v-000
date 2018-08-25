7791e57af90584efae05bd1312781fe61c922592
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-25 23:11:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["Larry, Moe, Curly, Schemp"]
+  array = ["Moe, Larry, Curly, Shemp"]
 end
 
 def add_element_to_end_of_array(array, element)
